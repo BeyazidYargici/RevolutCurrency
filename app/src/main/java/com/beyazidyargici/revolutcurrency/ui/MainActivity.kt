d5757386eca93fe5f5240dd038bf7cdc201c1659
BeyazidYargici <beyazidyargici@gmail.com>
2020-02-10 20:17:42 +0300

    - add libraries to gradle file, - update manifest file, - add resource file

---
+++ app/src/main/java/com/beyazidyargici/revolutcurrency/ui/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.beyazidyargici.revolutcurrency.ui
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.commit
+import com.beyazidyargici.revolutcurrency.R
+import com.beyazidyargici.revolutcurrency.base.BaseActivity
+import com.beyazidyargici.revolutcurrency.ui.currencyrates.CurrencyRatesFragment
+import com.beyazidyargici.revolutcurrency.util.CustomAlertDialog
+import com.beyazidyargici.revolutcurrency.util.DialogInterface
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import javax.inject.Inject
+
+class MainActivity : BaseActivity(), DialogInterface {
+
+    var context: Context? = null
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
+
+    override fun getLayout(): Int =
+        R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context = this
+        supportFragmentManager.commit(allowStateLoss = true) {
+            val fragment = CurrencyRatesFragment()
+            replace(R.id.ac_main_fragment_container, fragment, fragment.tag)
+        }
+    }
+    private val customAlertDialog = CustomAlertDialog(this)
+
+    override fun showDialog() {
+        customAlertDialog.createAlertDialog()
+    }
+
+
+}
